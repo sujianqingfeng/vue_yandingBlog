e7c799787beeb54cc6e954c60872da1dbd9e2130
sujian <121116111@qq.com>
2018-04-16 20:48:48 +0800

    update:登录

--- src/components/user/Login.vue
+++ src/components/user/Login.vue
@@ -12,10 +12,11 @@
       <div class="md-layout-item md-small-size-100 md-xsmall-size-100 blog-login-left-warpper">
 
       </div>
-      <div class="md-layout-item md-small-size-100 md-xsmall-size-100 blog-login-right-warpper  md-alignment-center">
+      <div class="md-layout-item md-small-size-100 md-xsmall-size-100 blog-login-right-warpper md-layout md-alignment-center-center">
 
+        <div class="md-layout-item">
 
-          <div class="blog-login-header md-layout">
+          <div class="blog-login-header md-layout-item md-layout">
             <div class="md-layout-item blog-login-label-warpper">
               <p>注册</p>
               <p class="md-title">登录</p>
@@ -29,20 +30,23 @@
             </div>
           </div>
 
-          <div class="blog-login-form-warpper">
+          <div class="blog-login-form-warpper md-layout md-alignment-center-center">
 
-            <md-field md-clearable>
-              <label class="login-label">用户名</label>
-              <md-input class="login-input" v-model="params.username"></md-input>
-            </md-field>
+            <div class="md-layout-item">
 
-            <md-field>
-              <label class="login-label">密码</label>
-              <md-input v-model="params.password" type="password"></md-input>
-            </md-field>
+              <md-field md-clearable>
+                <label class="login-label">用户名</label>
+                <md-input class="login-input" v-model="params.username"></md-input>
+              </md-field>
+
+              <md-field>
+                <label class="login-label">密码</label>
+                <md-input v-model="params.password" type="password"></md-input>
+              </md-field>
+            </div>
 
           </div>
-
+        </div>
 
       </div>
 
@@ -72,7 +76,7 @@ export default {
 </script>
 
 
-<style>
+<style scope>
 .blog-login-warpper {
   max-width: 900px;
   margin: 0 auto;
@@ -81,23 +85,75 @@ export default {
 }
 
 .blog-login-left-warpper {
-  background-image: url("timg22.jpg");
+  background-image: url("16pic_4760725_b.jpg");
 }
 
 .blog-login-right-warpper {
-  background-color: #26A5B3;
+  background-color: #AABCD4;
   padding: 18px;
-  color: white
+  color: white;
 }
 
 .blog-login-form-warpper {
+  margin-top: 50px;
   width: 100%;
+  height: 100%;
 }
 
-.login-label{
-  color: white !important
+.login-label {
+  color: white !important;
 }
-.login-input{
-   color: white !important
+.login-input {
+  color: white !important;
 }
+
+.md-icon-font svg {
+  color: white !important;
+  fill: white !important;
+}
+
+.md-field input {
+  color: white !important;
+  text-shadow: 0px 0px 0px white;
+  -webkit-text-fill-color: white !important;
+  outline-color: white !important;
+}
+
+.md-field input ::before {
+  color: white !important;
+  text-shadow: 0px 0px 0px white;
+  -webkit-text-fill-color: white !important;
+  outline-color: white !important;
+}
+
+.md-field input ::after {
+  color: white !important;
+  text-shadow: 0px 0px 0px white;
+  -webkit-text-fill-color: white !important;
+}
+
+.md-field input :focus {
+  outline-color: white !important;
+   outline-offset: -5px  !important;
+   outline-width: 10px;
+   border-bottom-color: white !important;
+}
+
+.md-field ::before{
+
+}
+
+.md-field.md-them-default.md-focused .md-input{
+ color: #fff !important;
+}
+
+:root{
+  --md-theme-default-primary:white !important;
+  --md-teheme-default-accent:white  !important
+}
+
+.md-field.md-theme-default:after{
+   background-color: white !important
+ }
+
 </style>
