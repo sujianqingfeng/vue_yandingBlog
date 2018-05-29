95404c4b50ec0cd7003a33ed028d4ce5d6b84c47
sujian <121116111@qq.com>
2018-05-29 18:15:14 +0800

    update:加入又拍云logo部分

--- src/components/index/Introduce.vue
+++ src/components/index/Introduce.vue
@@ -1,48 +1,34 @@
-<template>
-    <v-app>
-        <v-content>
-
-            <v-container class="blog-container">
-                <v-card>
-                    <v-layout class="blog-header-height blog-header-bg display-1"  d-flex align-end px-3 py-3 >
-                        <span class="introduce-title">项目介绍</span>
-                    </v-layout>
-
-                    <v-layout px-3 py-3>
-                        本着没事干，练手的原因，加之写到中途，又舍不得放弃，这个项目就应运而生了。
-                    </v-layout>
-
-                    <v-layout px-3 py-3>
-                        然后...
-                    </v-layout>
-
-                     <v-layout px-3 py-3>
-                        然后...
-                    </v-layout>
-
-                     <v-layout px-3 py-3>
-                        不知道怎么写了
-                    </v-layout>
-
-                </v-card>
-            </v-container>
-
-        </v-content>
-    </v-app>
+<template lang='pug'>
+    v-app
+        v-content
+            v-container.blog-container
+                v-card
+                    v-layout.blog-header-height.blog-header-bg.display-1(d-flex,align-end,px-3,py-3)
+                        span.introduce-title 项目介绍
+                    v-layout(px-3,py-3) 本着没事干，练手的原因，加之写到中途，又舍不得放弃，这个项目就应运而生了。
+                    v-layout(px-3,py-3) 然后...
+                    v-layout(px-3,py-3) 然后...
+                    v-layout(px-3,py-3) 不知道怎么写了
+        v-footer(height="180px") 
+            Footer
 
 </template>
 <script>
+
+import Footer from './IntroduceFooter'
 export default {
   name: 'introduce',
   data: () => ({
 
-  })
+  }),
+  components: {
+    Footer
+  }
 }
 </script>
 
-<style scoped>
-
-.introduce-title{
+<style scoped lang='stylus'>
+.introduce-title
     color: white;
-}
+
 </style>
---
+++ src/components/index/IntroduceFooter.vue
@@ -0,0 +1,37 @@
+<template lang='pug'>
+    v-container(py-0)
+        v-layout(align-center)
+            v-flex(xl12,xm12,md3)
+                v-btn(icon)
+                    v-icon mdi-github-circle
+                v-btn(icon)
+                    v-icon mdi-email-outline
+                v-btn(icon)
+                    v-icon mdi-qqchat
+                v-btn(icon)
+                    v-icon mdi-link-variant
+            v-flex(d-flex,justify-center,xl12,xm12,md6) Copyright  2018 YanDing's Blog
+            v-flex(xl12,xm12,md3) 
+                v-layout(column,align-end)
+                    v-flex Powered by Sujian
+                    v-flex(d-flex,align-center)
+                        span 托管于
+                        icon(name='a',:scale="20")
+                        //- img.youpaiyun(src='~assets/imgs/youpaiyun.png')
+</template>
+
+<script>
+export default {
+  name: 'introduce-footer',
+  data: () => ({
+
+  })
+}
+</script>
+
+<style scoped lang='stylus'>
+    .youpaiyun
+        width 80px
+        height 40px
+</style>
+
