aaaafe66368fe051bb01905ea19f36379517fc04
sujian <121116111@qq.com>
2018-06-03 17:49:33 +0800

    update: 评论回复组件 部分

---
+++ src/components/blog-detail/BlogReviewReply.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+    v-container 
+        v-tabs(slider-color="black")
+            v-tab(ripple) 回复    
+            v-tab(ripple) 预览
+            v-tab-item
+                v-text-field(textarea,label='评论内容',color='grey',v-model='reply',rows=5)
+            v-tab-item
+                v-container.display(v-html='md2html?md2html:"没有评论内容"')
+        v-layout
+            v-flex(d-flex,align-center)
+                span.caption 支持markdown语法
+            v-btn(color='info',@click='submit') 发表
+</template>
+
+<script>
+import { mavonEditor } from 'mavon-editor'
+
+export default {
+  name: 'review-reply',
+  props: {
+
+  },
+  data: () => ({
+    reply: '',
+    md2htmlstr: ''
+  }),
+  methods: {
+    submit () {
+
+    }
+  },
+
+  computed: {
+    md2html: function () {
+      let md = mavonEditor.getMarkdownIt()
+      return md.render(this.reply)
+    }
+  }
+}
+</script>
+
+
+
+<style lang="stylus" scoped>
+.display 
+    height 200px
+    overflow-y auto
+
+</style>
+
+
+
